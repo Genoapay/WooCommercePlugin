9fc6c9fe16a32407de9d753908e8524b4e4f6ab3
genoapay-francine <francine@genoapay.com>
2019-11-12 12:04:08 +1300

    Added exception handling to genoapay files too

--- woocommerce-gateway-latitudepay/includes/class-woocommerce-gateway-latitudepay.php
+++ woocommerce-gateway-latitudepay/includes/class-woocommerce-gateway-latitudepay.php
@@ -318,13 +318,13 @@ class WooCommerce_Gateway_Genoapay extends WC_Payment_Gateway {
 		global $product;
 		if ( is_checkout() ) {
 			return ( ( $this->minimum_amount > 0 && $this->minimum_amount > WC()->cart->get_displayed_subtotal() ) || $this->maximum_amount < WC()->cart->get_displayed_subtotal() );
-		} elseif( is_product() && is_object($product)) {
-		    try {
+		} elseif (is_product() && is_object($product)) {
+            try {
                 return (($this->minimum_amount > 0 && $this->minimum_amount > $product->get_price()) || $this->maximum_amount < $product->get_price());
             } catch (Exception $e) {
-		        return false;
+                return false;
             }
-		}
+        }
 
 	}
 
